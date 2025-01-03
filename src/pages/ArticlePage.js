562b2da89665040a925980e5c6a072054d39b9ee
Jayron <jayronsanchez@gmail.com>
2025-01-03 14:18:07 +0800

    Added authentication

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -8,7 +8,12 @@ import AddCommentForm from "../components/AddCommentForm";
 import useUser from "../hooks/useUser";
 
 const ArticlePage = () => {
-  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  const [articleInfo, setArticleInfo] = useState({
+    upvotes: 0,
+    comments: [],
+    canUpvote: false,
+  });
+  const { canUpvote } = articleInfo;
   const { user, isLoading } = useUser();
 
   // gets the url parameter
@@ -17,19 +22,30 @@ const ArticlePage = () => {
   useEffect(() => {
     // create an async function inside the useEffect hook to allow await axios and update the local state here
     const loadArticleInfo = async () => {
+      const token = user && (await user.getIdToken());
+      const headers = token ? { authtoken: token } : {};
       // you must add proxy property in package.json to allow requests to port 8000
-      const response = await axios.get(`/api/articles/${articleId}`);
+      const response = await axios.get(`/api/articles/${articleId}`, {
+        headers,
+      });
       const articleInfo = response.data;
       setArticleInfo(articleInfo);
     };
-
-    loadArticleInfo();
-  }, [articleId]);
+    if (!isLoading) {
+      loadArticleInfo();
+    }
+  }, [articleId, user]);
 
   const article = articles.find((article) => article.name === articleId);
 
   const addUpvote = async () => {
-    const response = await axios.put(`/api/articles/${articleId}/upvote`);
+    const token = user && (await user.getIdToken());
+    const headers = token ? { authtoken: token } : {};
+    const response = await axios.put(
+      `/api/articles/${articleId}/upvote`,
+      null,
+      { headers }
+    );
     const updatedArticle = response.data;
     setArticleInfo(updatedArticle);
   };
@@ -43,7 +59,11 @@ const ArticlePage = () => {
       <h1>{article.title}</h1>
       <div className="upvotes-section">
         {user ? (
-          <button onClick={addUpvote}>Upvote</button>
+          canUpvote ? (
+            <button onClick={addUpvote}>Upvote</button>
+          ) : (
+            <button>Already Upvoted</button>
+          )
         ) : (
           <button>Log in to upvote</button>
         )}
